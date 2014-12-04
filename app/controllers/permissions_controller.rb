63ef7aeab28f30366b9d90e2f835bc44f5487684
Peter O'Malley <ooomalley@gmail.com>
2014-12-03 20:32:47 -0800

    permission controller tests passing.
    placeholders for views/permissions/create|destroy

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,16 +1,47 @@
 class PermissionsController < ApplicationController
   before_action :signed_in_user
+  before_action :can_create, only: [:create]
+  before_action :can_destroy, only: [:destroy]
 
   def create
-
+    #@permission = Task.find(permission_params[:task_id]).permissions.build(permission_params)
+    @permission = Permission.new permission_params
+    if @permission.save
+      respond_to do |format|
+        format.html { redirect_back_or root_url }
+        format.js
+      end
+    else
+      render status: :forbidden
+    end
   end
 
   def destroy
+    if Permission.find(params[:id]).destroy
+      respond_to do |format|
+        format.html { redirect_back_or root_url }
+        format.js
+      end
+    else
+      render status: :forbidden
+    end
 
   end
 
   private
   def permission_params
     params.require(:permission).permit(:task_id, :user_id, :owner, :editor, :viewer)
+  end
+
+  def can_create
+    unless current_user.owner? Task.find(params[:permission][:task_id])
+      render status: :forbidden
+    end
+  end
+
+  def can_destroy
+    unless current_user.owner?(Permission.find_by_id(params[:id]).task) || Permission.find_by_id(params[:id]).user == current_user
+      render status: :forbidden
+    end
   end
 end
--- spec/controllers/permissions_controller_spec.rb
+++ spec/controllers/permissions_controller_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe PermissionsController do
 
-  let(:user_a)  { FactoryGirl.create(:user_with_tasks) }
-  let(:user_b)  { FactoryGirl.create(:user_with_tasks) }
-  let(:task)    { user_a.tasks.first }
+  # need to use let! because otherwise the lazy-evaluation was causing
+  # the change in Permission.count to be miscounted (I think!)
+  let!(:user_a)  { FactoryGirl.create(:user_with_tasks) }
+  let!(:user_b)  { FactoryGirl.create(:user_with_tasks) }
+  let!(:task)    { user_a.tasks.first }
 
   before {
     sign_in user_a, no_capybara: true
@@ -13,11 +15,11 @@ describe PermissionsController do
   describe 'adding a permission with AJAX' do
     it 'should create the permission' do
       expect do
-        xhr :post, :create, permission: { task: task, user: user_b, editor: true }
+        xhr :post, :create, permission: { task_id: task.id, user_id: user_b.id, editor: true }
       end.to change(Permission, :count).by 1
     end
     it 'should respond with success' do
-      xhr :post, :create, permission: { task: task, user: user_b, editor: true }
+      xhr :post, :create, permission: { task_id: task.id, user_id: user_b.id, editor: true }
       expect(response).to be_success
     end
   end
@@ -41,14 +43,17 @@ describe PermissionsController do
   describe 'for non-owners' do
     before {
       sign_in user_b, no_capybara: true
+      task.permissions.create!(user: user_b, editor: true)
     }
+    let(:perm) { task.permissions.find_by(user: user_a) }
+    let(:perm_b) { task.permissions.find_by(user: user_b) }
     it 'should not allow task creation' do
       expect do
-        xhr :post, :create, permission: { task: task, user: user_b, owner: true }
+        xhr :post, :create, permission: { task_id: task.id, user_id: user_b.id, owner: true }
       end.not_to change(Permission, :count)
     end
     it 'create should respond with failure' do
-      xhr :post, :create, permission: { task: task, user: user_b, owner: true }
+      xhr :post, :create, permission: { task_id: task.id, user_id: user_b.id, owner: true }
       expect(response).not_to be_success
     end
     it 'should not allow task deletion' do
@@ -59,6 +64,11 @@ describe PermissionsController do
     it 'destroy should respond with failure' do
       xhr :post, :destroy, id: perm.id
       expect(response).not_to be_success
+    end
+    it 'should allow deletion of own permission' do
+      expect do
+        xhr :post, :destroy, id: perm_b.id
+      end.to change(Permission, :count).by(-1)
     end
   end
 
